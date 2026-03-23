a70bddb3778e8da633ae42fd5b77f0469392d95d
christianhw27 <dannyychr27@gmail.com>
2026-03-23 20:16:20 +0700

    benerin firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-// main.dart
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/theme.dart';
+import 'firebase_options.dart';
 import 'screens/onboarding_screen.dart'; // Import onboarding
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FoodCourtApp());
 }
 
@@ -16,7 +21,8 @@ class FoodCourtApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Food Court Plus+',
       theme: AppTheme.lightTheme,
-      home: const OnboardingScreen(), // <-- Arahkan ke OnboardingScreen
+      home:
+          const OnboardingScreen(), // <-- Arahkan ke OnboardingScreenjdbjasbdjasbdj
     );
   }
 }
